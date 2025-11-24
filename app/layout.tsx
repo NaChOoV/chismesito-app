b82197bf8104edd96b9dae0e58c49fa326b8db2c
NaChOoV <fuenzalida.veas@gmail.com>
2025-11-23 23:50:58 -0300

    feat: separate viewport configuration from metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
     title: 'Chismesito App',
     description: 'App de chismes',
-    viewport: {
-        width: 'device-width',
-        initialScale: 1,
-        maximumScale: 1,
-        userScalable: false,
-    },
+};
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
 };
 
 const geistSans = Geist({
