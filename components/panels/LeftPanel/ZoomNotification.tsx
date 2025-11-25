14f2f5fe301ef7fe911279f3c5687b46bcb73adc
NaChOoV <fuenzalida.veas@gmail.com>
2025-11-24 22:27:47 -0300

    feat: update ZoomNotification messages for clarity and improve text responsiveness

--- components/panels/LeftPanel/ZoomNotification.tsx
+++ components/panels/LeftPanel/ZoomNotification.tsx
@@ -6,7 +6,9 @@ import { useMapContext } from '@/context/MapContext';
 export default function ZoomNotification() {
     const { canSeeGossip } = useMapContext();
 
-    const message = canSeeGossip ? 'Puedes ver chismes' : 'Haz zoom para ver chismes';
+    const message = canSeeGossip
+        ? 'Puedes ver y agregar chismes'
+        : 'Haz zoom para ver y agregar chismes';
     const backgroundColor = canSeeGossip ? 'bg-green-800' : 'bg-red-800';
     const icon = canSeeGossip ? (
         <EyeIcon className="inline-block w-4 h-4 text-white" />
@@ -19,7 +21,7 @@ export default function ZoomNotification() {
             className={`${backgroundColor} px-4 py-1 rounded-md flex items-center align-middle gap-2`}
         >
             {icon}
-            <div className={`text-white text-lg`}>{message}</div>
+            <div className={`text-white md:text-lg sm:text-sm`}>{message}</div>
         </div>
     );
 }
